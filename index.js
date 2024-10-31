f8b241785697a581017fa1c7a544cd05d7c244cd
DmitriyALEX <doiroffical@gmail.com>
2024-10-31 18:14:42 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+const { PrismaClient } = require('@prisma/client')
+
+const httpServer = createServer()
+
+const prisma = new PrismaClient()
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+})
+
+io.on('connection', socket => {
+    console.log('user connected', socket.id)
+
+    socket.on('message', msg => {
+        if (msg) {
+            const createMessage = async () => {
+                const newMessage = await prisma.message.create({
+                    data: {
+                        user: {
+                            connect: { id: msg.fromUserId },
+                        },
+                        fromUserId: msg.fromUserId,
+                        toUserId: msg.toUserId,
+                        message: msg.message,
+                    },
+                })
+                newMessage
+
+                //send msg to back to client
+                socket.emit('message', newMessage)
+
+                //for realtime chat
+                //send msg to all users which in this connection
+                io.emit('message', newMessage)
+            }
+            createMessage()
+        }
+
+        //send msg to back to client
+        // socket.emit('message', msg)
+
+        //for realtime chat
+        //send msg to all users which in this connection
+        // io.emit('message', msg)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconected', socket.id)
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+httpServer.listen(PORT, () => {
+    console.log('listen port')
+})
